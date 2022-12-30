e10fd9465eb6441c5c09d497ea0e201a56d8c7bc
ElvisStwar <elvis.stwar@hotmail.com>
2022-12-30 10:16:17 -0500

    cracion de modulos y componenetes

---
+++ src/app/registro/registro.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DatosComponent } from './datos/datos.component';
+
+
+
+@NgModule({
+  declarations: [
+    DatosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RegistroModule { }
